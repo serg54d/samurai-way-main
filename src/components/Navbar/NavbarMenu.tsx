521ab631ca7018f9aaf5dc3079297876f4427dc4
sadunaev <serg54d@bk.ru>
2024-04-25 02:31:42 +0300

    use NavLink

--- src/components/Navbar/NavbarMenu.tsx
+++ src/components/Navbar/NavbarMenu.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import s from './NavbarMenu.module.css'
+import { NavLink } from 'react-router-dom';
+
+
+type ActiveClassNameType = {
+	activeStyle: string
+}
+
+
 
 export const NavbarMenu = () => {
 	return (
 		<nav className={s.navbar}>
 			<ul className={s.list}>
 				<li className={s.item}>
-					<a href="/profile">Profile</a>
+					<NavLink className={navData => navData.isActive ? s.active : s.item} to="/profile">Profile</NavLink>
 				</li>
 				<li className={s.item}>
-					<a href="/dialogs">Messages</a>
+					<NavLink className={navData => navData.isActive ? s.active : s.item} to="/dialogs">Messages</NavLink>
 				</li>
 				<li className={s.item}>
-					<a href="/news">News</a>
+					<NavLink className={navData => navData.isActive ? s.active : s.item} to="/news">News</NavLink>
 				</li>
 				<li className={s.item}>
-					<a href="/music">Music</a>
+					<NavLink className={navData => navData.isActive ? s.active : s.item} to="/music">Music</NavLink>
 				</li>
 				<li className={s.item}>
-					<a href="/settings">Settings</a>
+					<NavLink className={navData => navData.isActive ? s.active : s.item} to="/settings">Settings</NavLink>
 				</li>
 			</ul>
-		</nav>
+		</nav >
 	)
 }
