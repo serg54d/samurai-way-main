405404a80bc08bbdbec18428d044c88a5ad275c8
sadunaev <serg54d@bk.ru>
2024-04-25 23:54:01 +0300

    create page dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -7,8 +7,43 @@ type DialogsPropsType = {
 
 export const Dialogs = (props: DialogsPropsType) => {
 	return (
-		<div>
-			Dialogs
+		<div className={s.dialogsPage}>
+			<h2 className={s.title}>Dialogs</h2>
+			<div className={s.wrapper}>
+
+
+				<div className={s.dialogs}>
+
+					<ul className={s.dialogsList}>
+						<li className={s.dialogsItem}>Serj</li>
+						<li className={s.dialogsItem}>Kate</li>
+						<li className={s.dialogsItem + ' ' + s.active}>Alina</li>
+						<li className={s.dialogsItem}>Aleks</li>
+						<li className={s.dialogsItem}>Pablo</li>
+					</ul>
+				</div>
+				<div className={s.messages}>
+					<ul className={s.messagesList}>
+						<li className={s.messagesItem}>
+							message
+						</li>
+						<li className={s.messagesItem}>
+							message
+						</li>
+						<li className={s.messagesItem}>
+							message
+						</li>
+						<li className={s.messagesItem}>
+							message
+						</li>
+						<li className={s.messagesItem}>
+							message
+						</li>
+					</ul>
+				</div>
+
+			</div>
 		</div>
+
 	)
 }
